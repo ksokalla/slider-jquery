90bf04b4e3058d7e2667aec70786ddbd91a6d6e2
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-17 23:39:28 +0100

    tyle zrobiłem w piątek

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 'use strict';
 
+$(function() {
+	var slider = $('#slider');
+	var slideshow = $('.slide-show');
+	var slideCount = $('.slide-show').children().length;
+	var slideWidth = $(100/slideCount);
+	var slideIndex = $('.single-slide').index('.single-slide');
+	
+	$('.slide-show').css({'width': '500%'});
+						  
+	$('body').find('.single-slide').each(function() {
+						  $('.single-slide').css({'margin-left': 'slideWidth*slideIndex',
+												  'width': '20%'});
+						 });
+	$('.prev-slide').on('click', function slide(slideIndex - 1){
+		
+	});
+	
+	$('.next-slide').on('click', function slide(slideIndex + 1){
+		
+	});
+
+});
+console.log(slider);
