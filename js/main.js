d17496e35552fcb52d11d669449cc3ccb099e882
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-18 10:22:56 +0100

    poprawki na zajęciach

--- js/main.js
+++ js/main.js
@@ -4,17 +4,18 @@ $(function() {
 	var slider = $('#slider');
 	var slideshow = $('.slide-show');
 	var slideCount = $('.slide-show').children().length;
-	var slideWidth = $(100/slideCount);
+	var slideWidth = (100/slideCount);
 	var slideIndex = $('.single-slide').index('.single-slide');
 	
-	$('.slide-show').css({'width': '500%'});
+	$('.slide-show').css({'width': slideCount*100 + '%'});
 						  
-	$('body').find('.single-slide').each(function() {
-						  $('.single-slide').css({'margin-left': 'slideWidth*slideIndex',
-												  'width': '20%'});
-						 });
-	function slide(newSlideIndex) {
+	$('.slide-show').find('.single-slide').each(function(slideIndex) {
+		console.log(slideWidth);
+		$(this).css({'left': slideWidth*slideIndex + '%' });
+		$(this).css({'width': slideWidth+'%'});
+	});
+//	function slide(newSlideIndex) {
 		
-	}
+	
 });
 console.log(slider);
