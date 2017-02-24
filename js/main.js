02e5186f5b1d16a73259c53cd0993e74d921f3ef
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-24 16:41:49 +0100

    w domu start

--- js/main.js
+++ js/main.js
@@ -5,17 +5,19 @@ $(function() {
 	var slideshow = $('.slide-show');
 	var slideCount = $('.slide-show').children().length;
 	var slideWidth = (100/slideCount);
-	var slideIndex = $('.single-slide').index('.single-slide');
+	var slideIndex = $('.single-slide').index();
 	
 	$('.slide-show').css({'width': slideCount*100 + '%'});
 						  
 	$('.slide-show').find('.single-slide').each(function(slideIndex) {
-		console.log(slideWidth);
+		
 		$(this).css({'left': slideWidth*slideIndex + '%' });
 		$(this).css({'width': slideWidth+'%'});
 	});
-//	function slide(newSlideIndex) {
-		
-	
+					
+		function slide (newSlideIndex) {
+			if (newSlideIndex == 0 || newSlideIndex == 5) {
+				break;
+			}
+		}
 });
-console.log(slider);
