060e0ff020665eb67bd08ff57d50aadb0d3de5e3
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-03-03 21:23:37 +0100

    porpawiona od mateusza

--- js/main.js
+++ js/main.js
@@ -1,49 +1,52 @@
 'use strict';
-var slider = $('#slider');
-var slideshow = $('.slide-show');
-var slideCount = $('.slide-show').children().length;
-var slideWidth = (100 / slideCount);
-var slideIndex = $('.single-slide').index();
-var slideCaption = $('.slider-caption').text();
-var marginLeft = (slideIndex * -100) + "%";
-$('.slider-caption').hide('fast');
-
-
-function slide(newSlideIndex) {
-	function callback() {
-		slideIndex = $(newSlideIndex);	
-	}
-	
-	$('.slide-show').animate({'margin-left': marginLeft}, 2000, callback);
-	console.log("dziala");
-}
-
-$('h3').click(function() {
-		$(this).animate({'font-size': '3em', 'margin-left': '100px', 'color': 'blue'}, 2000, animacja);
-	});
-
-$(function () {
-	$('.slide-show').css({
-		'width': slideCount * 100 + '%'
-	});
-	$('.slide-show').find('.single-slide').each(function (slideIndex) {
-		$(this).css({
-			'left': slideWidth * slideIndex + '%'
-		});
-		$(this).css({
-			'width': slideWidth + '%'
-		});
-	});
-	$('.prev-slide').on('click', slide); {
-		if (slideIndex > 0) {
-			var slideIndex = slideIndex-1;
-		}
-	}
-	$('.next-slide').on('click', slide); {
-		if (slideIndex < 4) {
-			var slideIndex = slideIndex+1;
-		}
-	}
-
-	
-});
+$(function(){
+    
+    var slider = $('#slider');
+    //console.log(slider);
+    var slideshow=$('.slide-show');
+    //console.log(slideshow);
+    var slideCount=slideshow.children().length;
+    //console.log(slideCount);
+    var slideWidth=100/slideCount+"%";
+    var slideIndex=0;
+    
+    $('.slide-show').css("width", 100*slideCount+"%");
+    $('.slide-show').find('.single-slide').each(function(i){
+        $(this).css( { width: slideWidth } );
+        $(this).css( { marginLeft :parseInt(slideWidth)*i+"%"} );
+    });
+    
+    var currentSlide=0;
+    
+    function slide(newSlideIndex){
+        currentSlide+=newSlideIndex;
+        if(currentSlide==0 || currentSlide<0){
+            currentSlide=0;
+            console.log(currentSlide);
+        }else if(currentSlide==slideshow.children().length-1||currentSlide>slideshow.children().length-1){
+            currentSlide=slideshow.children().length-1;
+            console.log(currentSlide);
+        }else{
+            console.log(currentSlide);
+        }
+        
+        var slideCaption= $('.single-slide:nth-child('+(currentSlide+1)+')>p');
+        slideCaption.hide();
+        
+        var marginLeft= currentSlide*-100+"%";
+        $('.slide-show').animate({'margin-left': marginLeft},1000, function(){
+          slideCaption.fadeIn(500);
+        })   
+    }
+    
+    $( ".prev-slide" ).click(function(event){
+        event.preventDefault();
+        slide(-1);
+    });
+    $( ".next-slide" ).click(function(event){
+        event.preventDefault();
+        slide(1);
+    });
+    
+    
+})
